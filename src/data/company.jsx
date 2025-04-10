84c913a514ba2da82c33c01d4b1d6688e8178422
KnowledgeWhite <motebang.mosoeu@gmail.com>
2025-04-10 18:11:31 +0100

    mockup placeholder

---
+++ src/data/company.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const CompaniesData = () => {
+  const companies = [
+    //Placeholder mockup data
+    { id: 1, name: "Alice", email: "alice@example.com" },
+    { id: 2, name: "Bob", email: "bob@example.com" },
+    { id: 3, name: "Charlie", email: "charlie@example.com" },
+  ];
+};
+
+export default CompaniesData;
